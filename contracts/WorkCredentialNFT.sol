18a018a09a810dbce21ba7c2d3e837956af60aea
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-07-16 16:13:26 +0900

    added documentation comments

--- contracts/WorkCredentialNFT.sol
+++ contracts/WorkCredentialNFT.sol
@@ -2,38 +2,51 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+/**
+ * @title WorkCredentialNFT
+ * @dev An ERC-721 compliant NFT contract with additional functionalities for administrative control and enumeration.
+ */
 contract WorkCredentialNFT is
     ERC721,
+    ERC721Enumerable,
     Pausable,
-    Ownable,
     AccessControl,
-    ERC721Enumerable
+    Ownable
 {
     using Counters for Counters.Counter;
     using Strings for uint256;
-    Counters.Counter private _tokenIdCounter;
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     struct TokenData {
         address minterAddress;
         string description;
     }
 
+    /**
+     * @dev Modifier to restrict access to admin only.
+     */
     modifier onlyAdmin() {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not a admin");
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
         _;
     }
+
     mapping(uint256 => TokenData) private _tokenData;
     string public _imageUrl =
         "https://gateway.pinata.cloud/ipfs/QmWJhNrFAQSFkT8svf4QC9mZdA7wkpcN5DRqkL2GXHLRkn";
 
+    /**
+     * @dev Sets the image URL of the NFTs.
+     * @param _url The new image URL.
+     */
     function setImageUrl(string memory _url) public onlyAdmin {
         _imageUrl = _url;
     }
@@ -42,6 +55,11 @@ contract WorkCredentialNFT is
         _setupRole(ADMIN_ROLE, admin);
     }
 
+    /**
+     * @dev Mints a single NFT and assigns it to the given address.
+     * @param minterAddress The address to whom the NFT will be minted.
+     * @param description The description of the NFT.
+     */
     function mint(
         address minterAddress,
         string memory description
@@ -55,6 +73,11 @@ contract WorkCredentialNFT is
         tokenData.description = description;
     }
 
+    /**
+     * @dev Mints multiple NFTs in a batch and assigns them to the given addresses.
+     * @param minterAddresses The addresses to whom the NFTs will be minted.
+     * @param descriptions The descriptions of the NFTs.
+     */
     function mintBatch(
         address[] memory minterAddresses,
         string[] memory descriptions
@@ -75,6 +98,10 @@ contract WorkCredentialNFT is
         }
     }
 
+    /**
+     * @dev Returns the token URI with additional metadata.
+     * @param tokenId The ID of the token.
+     */
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
@@ -110,6 +137,13 @@ contract WorkCredentialNFT is
             );
     }
 
+    /**
+     * @dev Performs actions before transferring tokens.
+     * @param from The address from which the tokens are being transferred.
+     * @param to The address to which the tokens are being transferred.
+     * @param tokenId The ID of the token being transferred.
+     * @param batchSize The size of the batch (not used in this contract).
+     */
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -123,10 +157,18 @@ contract WorkCredentialNFT is
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
+    /**
+     * @dev Burns an NFT by admin.
+     * @param tokenId The ID of the token to be burned.
+     */
     function burn(uint256 tokenId) external onlyAdmin {
         _burn(tokenId);
     }
 
+    /**
+     * @dev Checks if the contract supports the given interface.
+     * @param interfaceId The interface ID to check.
+     */
     function supportsInterface(
         bytes4 interfaceId
     )
@@ -142,31 +184,54 @@ contract WorkCredentialNFT is
             super.supportsInterface(interfaceId);
     }
 
+    /**
+     * @dev Pauses the contract by admin.
+     */
     function pause() public onlyAdmin {
         _pause();
     }
 
+    /**
+     * @dev Unpauses the contract by admin.
+     */
     function unpause() public onlyAdmin {
         _unpause();
     }
 
+    /**
+     * @dev Sets approval for all tokens (reverts to prevent usage for non-transferable NFTs).
+     * @param to The address for which the approval is set.
+     * @param approved True if the address is approved, false otherwise.
+     */
     function setApprovalForAll(
-        address,
-        bool
+        address to,
+        bool approved
     ) public virtual override(ERC721, IERC721) {
         revert(
             "You can't approve this NFT because this is Non Transferable NFT."
         );
     }
 
+    /**
+     * @dev Grants admin role to a new address by the owner.
+     * @param newadmin The address to which the admin role will be granted.
+     */
     function addAdmin(address newadmin) external onlyOwner {
         grantRole(ADMIN_ROLE, newadmin);
     }
 
+    /**
+     * @dev Revokes admin role from an address by the owner.
+     * @param admin The address from which the admin role will be revoked.
+     */
     function revoke_adminRole(address admin) external onlyOwner {
         revokeRole(ADMIN_ROLE, admin);
     }
 
+    /**
+     * @dev Gets the token IDs and metadata for tokens owned by the given address.
+     * @param owner The address for which the tokens are owned.
+     */
     function getTokensByAddress(
         address owner
     ) public view returns (uint256[] memory, string[] memory) {
