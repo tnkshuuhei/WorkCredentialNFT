f7ed274c7eda6875aba75f8b99574d781af7d734
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-07-16 15:18:12 +0900

    added batchmint

--- contracts/WorkCredentialNFT.sol
+++ contracts/WorkCredentialNFT.sol
@@ -12,46 +12,33 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract WorkCredentialNFT is ERC721, Pausable, Ownable, AccessControl {
     using Counters for Counters.Counter;
     using Strings for uint256;
-
-    string public _imageUrl =
-        "https://gateway.pinata.cloud/ipfs/QmWJhNrFAQSFkT8svf4QC9mZdA7wkpcN5DRqkL2GXHLRkn";
-    uint256 public constant MINT_PRICE = 0.001 ether;
-
     Counters.Counter private _tokenIdCounter;
-
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     struct TokenData {
         address minterAddress;
         string description;
     }
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     modifier onlyAdmin() {
         require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not a admin");
         _;
     }
     mapping(uint256 => TokenData) private _tokenData;
+    string public _imageUrl =
+        "https://gateway.pinata.cloud/ipfs/QmWJhNrFAQSFkT8svf4QC9mZdA7wkpcN5DRqkL2GXHLRkn";
+
+    function setImageUrl(string memory _url) public onlyAdmin {
+        _imageUrl = _url;
+    }
 
     constructor(address admin) ERC721("D-WorkCredentialNFT 2023", "DWC2023") {
         _setupRole(ADMIN_ROLE, admin);
     }
 
-    function setImageUrl(string memory _url) public onlyAdmin{
-        _imageUrl = _url;
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
     function mint(
         address minterAddress,
         string memory description
-    ) public payable whenNotPaused onlyAdmin{
-        require(msg.value == MINT_PRICE, "Error: Invalid value");
-
+    ) public payable whenNotPaused onlyAdmin {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
@@ -59,6 +46,26 @@ contract WorkCredentialNFT is ERC721, Pausable, Ownable, AccessControl {
         TokenData storage tokenData = _tokenData[tokenId];
         tokenData.minterAddress = minterAddress;
         tokenData.description = description;
+    }
+
+    function mintBatch(
+        address[] memory minterAddresses,
+        string[] memory descriptions
+    ) public payable whenNotPaused onlyAdmin {
+        require(
+            minterAddresses.length == descriptions.length,
+            "Input array lengths must be equal"
+        );
+
+        for (uint256 i = 0; i < minterAddresses.length; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(minterAddresses[i], tokenId);
+
+            TokenData storage tokenData = _tokenData[tokenId];
+            tokenData.minterAddress = minterAddresses[i];
+            tokenData.description = descriptions[i];
+        }
     }
 
     function tokenURI(
@@ -113,12 +120,6 @@ contract WorkCredentialNFT is ERC721, Pausable, Ownable, AccessControl {
         _burn(tokenId);
     }
 
-    function withdraw() external onlyOwner {
-        Address.sendValue(payable(owner()), address(this).balance);
-    }
-
-    function renounceOwnership() public override onlyOwner {}
-
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override(ERC721, AccessControl) returns (bool) {
@@ -126,5 +127,35 @@ contract WorkCredentialNFT is ERC721, Pausable, Ownable, AccessControl {
             interfaceId == type(IAccessControl).interfaceId ||
             interfaceId == type(IERC721).interfaceId ||
             super.supportsInterface(interfaceId);
+    }
+
+    function pause() public onlyAdmin {
+        _pause();
+    }
+
+    function unpause() public onlyAdmin {
+        _unpause();
+    }
+
+    function setApprovalForAll(address, bool) public virtual override {
+        revert(
+            "You can't approve this NFT because this is Non Transferable NFT."
+        );
+    }
+
+    function getTokensByAddress(
+        address owner
+    ) public view returns (uint256[] memory, string[] memory) {
+        uint256 balance = balanceOf(owner);
+        uint256[] memory tokenIds = new uint256[](balance);
+        string[] memory metadata = new string[](balance);
+
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(owner, i);
+            tokenIds[i] = tokenId;
+            metadata[i] = tokenURI(tokenId);
+        }
+
+        return (tokenIds, metadata);
     }
 }
